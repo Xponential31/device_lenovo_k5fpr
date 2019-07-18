b46c9d9c056546cfff20082b7c85f29e22a43636
RajatBillava <50406396+RajatBillava@users.noreply.github.com>
2019-07-18 18:41:00 +0530

    k5fpr: Update media codecs
    Signed-off-by: SahilSonar <sss.sonar2003@outlook.com>

--- device_k5fpr.mk
+++ device_k5fpr.mk
@@ -201,7 +201,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
